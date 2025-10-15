e4ec30ae07bc58abc0da850ab70c78f1bb78bfaf
Kushal2885 <kushalsoni2928@gmail.com>
2025-10-15 18:02:28 +0530

    Add files via upload

---
+++ LogicBox.py
@@ -0,0 +1,21 @@
+print("Welcome to the Pattern Generator and Number Analyzer!")
+
+choice = int(input("Choose an option:\n1. Generate Patterns \n2. Analyze Numbers \n3. Exit \nEnter your choice: "))
+
+if choice == 1:
+    print("Pattern:")
+    for i in range(1, 6):
+        print("* " * i)
+elif choice == 2:
+    start = int(input("Enter the starting number: "))
+    end = int(input("Enter the ending number: "))
+    sum = 0
+    for num in range(start, end + 1):
+        if num % 2 == 0:
+            print(num, "is Even")
+        else:
+            print(num, "is Odd")
+        sum += num
+    print("Sum of all numbers from", start, "to", end, "is:", sum)
+elif choice == 3:
+        print("Exiting the program. Goodbye!")
